c838cd1a1eb45eaf56cf2903c72bbbd2991fb841
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-17 17:41:02 +0100

    raw to object mappings for new tables

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,21 @@
-# player details : column name in Jeff Scakman csv
 player_csv_map = {
     'fname': 'X',
     'lname': 'X.1',
     'nationality': 'UNK',
     'dob': '19000000',
     'hand': 'U'
+}
+
+game_csv_map = {
+    'round': 'round',
+    'score': 'score',
+    'w_rank': 'winner_rank',
+    'l_rank': 'loser_rank'
+}
+
+tournament_csv_map = {
+    'name': 'tourney_name',
+    'start_date': 'tourney_date',
+    'surface': 'surface',
+    'level': 'tourney_level'
 }
