04404f973febcd1d1fb834e7fe03509e48ef0b1d
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-17 16:47:59 +0100

    score to int

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,5 +1,8 @@
+from typing import Tuple
 import pandas as pd
+import numpy as np
 from datetime import datetime
+import re
 
 from .db.tables import Player
 from .constants import player_csv_map
@@ -12,3 +15,32 @@ def raw_player_to_object(raw_player: pd.Series) -> Player:
                   dob=datetime.strptime(
                       str(raw_player[player_csv_map['dob']]), '%Y%m%d'),
                   hand=raw_player[player_csv_map['hand']])
+
+
+def score_to_int_data(score: str) -> Tuple[int, int, int, int]:
+    '''
+    Converts string score to int values representing games and sets won by winner and loser
+
+    args:
+        score: string score
+
+    returns:
+        WGames, WSets, LGames, LSets
+    '''
+    if pd.isnull(score):
+        return -1
+
+    # regex to remove brackets and the contnets within
+    score = re.sub(r'\([^)]*\)', '', score).strip()
+    set_score = score.split(' ')
+
+    try:
+        # must be at least 2 sets
+        assert len(set_score) >= 2
+        game_score = np.array([set_score_.split('-')
+                               for set_score_ in set_score], dtype=int)
+        # winner must win at least 12 games
+        assert game_score[:, 0].sum() >= 12
+    except:
+        return np.NaN  #  triggered if string in score ie W/O or RET or assertions not met
+    return game_score[:, 0].sum(), 2, game_score[:, 1].sum(), len(set_score) - 2
--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 from datetime import datetime
 
-from src.data_cleaning import raw_player_to_object
+from src.data_cleaning import raw_player_to_object, score_to_int_data
 from src.constants import player_csv_map
 
 
@@ -18,3 +19,14 @@ def test_raw_player_to_object():
     assert player_object.nationality == nationality
     assert player_object.dob == datetime.strptime(str(dob), '%Y%m%d')
     assert player_object.hand == hand
+
+
+def test_score_to_int_data():
+    # (Wgames, Wsets, Lgames, Lsets)
+    assert score_to_int_data('7-6(7) 6-0') == (13, 2, 6, 0)
+    # ensure invalid scores are handled
+    assert np.isnan(score_to_int_data('6-0 RET'))
+    assert np.isnan(score_to_int_data('6-0'))
+    assert np.isnan(score_to_int_data('1-0 1-0'))
+    # nan scores are handled by the model and denoted as -1
+    assert score_to_int_data(np.NaN) == -1
