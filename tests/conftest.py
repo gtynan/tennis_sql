57ec80781666a237e1c76322eae1dfbaf585f8a4
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-17 17:40:37 +0100

    game sample data

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.db.db import DBClient
 from src.db.tables import Base
-from src.data_scraping import get_raw_players
+from src.data_scraping import get_raw_players, get_raw_games
 
 
 TEST_DB = 'test_db'
@@ -15,10 +15,15 @@ def db_client():
     yield db_client
     # teardown delete all `Base` created test_db tables
     db_client.session.close()
-    for table in Base.metadata.sorted_tables:
+    for table in reversed(Base.metadata.sorted_tables):
         table.drop(db_client.engine)
 
 
 @pytest.fixture(scope='session')
 def sample_players() -> None:
     return get_raw_players(n_players=5)
+
+
+@pytest.fixture(scope='session')
+def sample_games() -> None:
+    return get_raw_games(year_from=2020, year_to=2020, n_games=5)
