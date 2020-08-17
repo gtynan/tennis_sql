472292c5721efec55b9544945d4d6c42ff29ddc7
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-17 13:25:16 +0100

    using data fixture

--- tests/test_data_scraping.py
+++ tests/test_data_scraping.py
@@ -7,9 +7,10 @@ from src.constants import player_csv_map
 
 
 @pytest.mark.slow
-def test_get_raw_players():
-    raw_players = get_raw_players(n_players=5)
+def test_get_raw_players(sample_players):
+    raw_players = sample_players
+    expected_columns = list(player_csv_map.values())
+
     assert len(raw_players) == 5
     assert isinstance(raw_players, pd.DataFrame)
-    expected_columns = list(player_csv_map.values())
     assert np.isin(expected_columns, raw_players.columns).all()
