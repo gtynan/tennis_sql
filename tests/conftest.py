0c9bb4973b3c365ff087a9c2f072c002a99dc222
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-17 13:24:17 +0100

    added db teardown and data fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,24 @@
 import pytest
+
 from src.db.db import DBClient
+from src.db.tables import Base
+from src.data_scraping import get_raw_players
+
+
+TEST_DB = 'test_db'
 
 
 @pytest.fixture(scope='session')
 def db_client():
     # db instance
-    return DBClient(db_name='test_db')
+    db_client = DBClient(db_name=TEST_DB)
+    yield db_client
+    # teardown delete all `Base` created test_db tables
+    db_client.session.close()
+    for table in Base.metadata.sorted_tables:
+        table.drop(db_client.engine)
 
 
 @pytest.fixture(scope='session')
-def session(db_client):
-    # single session to interact with db
-    return db_client._Session()
+def sample_players() -> None:
+    return get_raw_players(n_players=5)
