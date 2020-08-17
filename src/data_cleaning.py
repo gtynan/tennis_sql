7d28156e3804c1c5b5266659443373bb12494d0e
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-17 13:23:07 +0100

    patched int to str

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -10,5 +10,5 @@ def raw_player_to_object(raw_player: pd.Series) -> Player:
                   lname=raw_player[player_csv_map['lname']],
                   nationality=raw_player[player_csv_map['nationality']],
                   dob=datetime.strptime(
-                      raw_player[player_csv_map['dob']], '%Y%m%d'),
+                      str(raw_player[player_csv_map['dob']]), '%Y%m%d'),
                   hand=raw_player[player_csv_map['hand']])
