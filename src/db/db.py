b80ecdf7f90c7e1f886093b5ba6f6cf43dcac24b
gavin_tynan <gavin.tynan1@gmail.com>
2020-09-02 12:21:44 +0100

    removed old methods added add_player

--- src/db/db.py
+++ src/db/db.py
@@ -1,17 +1,9 @@
-from typing import List
-from datetime import datetime
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.exc import NoResultFound
 
 from ._secrets import db_config
-from .tables import Base, Player
-
-'''
-Seperating read and write responsibilies
-'''
+from ..db_models.base import BASE
+from ..db_models.player import Player
 
 
 class DBClient:
@@ -27,7 +19,7 @@ class DBClient:
         self.engine = create_engine(connection_str, echo=True)
 
         # any class inheriting Base that does not have a table in the db will have one generated for them
-        Base.metadata.create_all(self.engine)
+        BASE.metadata.create_all(self.engine)
 
         # creates session objects if more needed (used in tests mainly)
         self._Session = sessionmaker(bind=self.engine)
@@ -42,47 +34,10 @@ class CommandDB:
 
     def __init__(self, db_client: DBClient) -> None:
         self.db_client = db_client
-        # some writes require read to ensure no duplicates
-        self.query_db = QueryDB(db_client)
 
-    def create_players(self, players: List[Player], check_duplicates: bool = True) -> None:
+    def add_player(self, player: Player) -> None:
         '''
         Writes players to database
-
-        args:
-            check_duplicates: if true will not add players already in db
         '''
-        if check_duplicates:
-            # for each player check if exists, if not add
-            for player in players:
-                try:
-                    self.query_db.read_player(
-                        player.fname, player.lname, player.dob)
-                except NoResultFound:
-                    self.db_client.session.add(player)
-        else:
-            self.db_client.session.add_all(players)
+        self.db_client.session.add(player)
         self.db_client.session.commit()
-
-
-class QueryDB:
-    '''
-    Read side
-    '''
-
-    def __init__(self, db_client: DBClient):
-        self.db_client = db_client
-
-    def read_player(self, fname: str, lname: str, dob: datetime) -> Player:
-        '''
-        Gets player from db, raises error if none or more than one instance
-
-        args:
-            fname: first name
-            lname: last name
-            dob: date of birth
-        '''
-        return self.db_client.session.query(Player).\
-            filter(Player.fname == fname).\
-            filter(Player.lname == lname).\
-            filter(Player.dob == dob).one()
--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -1,25 +1,10 @@
 import pytest
-from datetime import datetime
+import datetime
 import numpy as np
 
-from sqlalchemy import inspect
-from sqlalchemy.orm.exc import MultipleResultsFound
-
-from src.constants import player_csv_map
+from src.db_models.player import Player
+from src.db.db import CommandDB
 from ..conftest import TEST_DB
-from src.db.db import CommandDB, QueryDB
-from src.db.tables import Player, _Game, WTA, ITF, Tournament
-from src.data.data_scraping import get_raw_players
-from src.data.data_cleaning import raw_player_to_object
-
-# tables to check for in TestDBClient test_schema
-TABLE_CLASSES = [Player(), _Game(), WTA(), ITF(), Tournament()]
-
-
-@pytest.fixture(scope='module')
-def query_db(db_client) -> QueryDB:
-    # shared by both TestCommand and TestQuery
-    return QueryDB(db_client)
 
 
 class TestDBClient:
@@ -30,64 +15,23 @@ class TestDBClient:
         '''
         assert db_client.engine.url.database == TEST_DB
 
-    def test_schema(self, db_client):
-        '''
-        Ensure all tables in the src/db/tables module with Base parent are in the database
-        '''
-        db_inspector = inspect(db_client.engine)
-        db_tables = db_inspector.get_table_names()
-
-        # ensure ecpetced tables present
-        assert np.isin(
-            [table.__tablename__ for table in TABLE_CLASSES],
-            db_tables).all()
-
-        # ensure table matches expected schema
-        for table_class in TABLE_CLASSES:
-            for db_column in db_inspector.get_columns(table_class.__tablename__):
-                assert hasattr(table_class, db_column['name'])
-
 
 class TestCommandDB:
 
-    @pytest.fixture(scope='class')
+    @pytest.fixture(scope='module')
     def command_db(self, db_client) -> CommandDB:
         return CommandDB(db_client)
 
-    @pytest.mark.slow
-    def test_create_players(self, command_db, query_db, sample_players):
-        '''
-        Gets players adds them twice and ensures no duplictaes were added.
-        '''
-        raw_players = sample_players
-        clean_players = raw_players.apply(
-            lambda x: raw_player_to_object(x), axis=1).to_list()
+    def test_add_player(self, db_client, command_db):
+        fname, lname, nation, dob, hand = 'Tom', 'Jones', 'IRE', datetime.date(2000, 1, 1), 'R'
+        player = Player(first_name=fname, last_name=lname, nationality=nation, dob=dob, hand=hand)
 
-        # no players will have been added due to conftest teardown thus no need to check
-        command_db.create_players(clean_players, check_duplicates=False)
+        command_db.add_player(player)
 
-        raw_players = get_raw_players(n_players=len(raw_players) + 1)
-        clean_players = raw_players.apply(
-            lambda x: raw_player_to_object(x), axis=1).to_list()
-
-        # check duplicates should ensure nothing added except last row (+1)
-        command_db.create_players(clean_players, check_duplicates=True)
-
-        # check all players added, raises error if not
-        for player in clean_players:
-            query_db.read_player(player.fname, player.lname, player.dob)
-
-
-class TestQueryDB:
-
-    def test_read_player(self, query_db, sample_players):
-        player = sample_players.iloc[0]
-
-        read_player = query_db.read_player(
-            fname=player[player_csv_map['fname']],
-            lname=player[player_csv_map['lname']],
-            dob=datetime.strptime(str(player[player_csv_map['dob']]), '%Y%m%d')
-        )
-        # check the two variables not given to read match expectecd
-        assert player[player_csv_map['nationality']] == read_player.nationality
-        assert player[player_csv_map['hand']] == read_player.hand
+        # will raise exception if there is anything other than 1 instance present
+        db_client.session.query(Player).\
+            filter(Player.first_name == fname).\
+            filter(Player.last_name == lname).\
+            filter(Player.nationality == nation).\
+            filter(Player.dob == dob).\
+            filter(Player.hand == hand).one()
