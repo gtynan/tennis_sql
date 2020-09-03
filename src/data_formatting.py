4886616d1459873bdc2891afabb52f66858b9a34
gavin_tynan <gavin.tynan1@gmail.com>
2020-09-03 11:09:17 +0100

    format tournament

--- src/data_formatting.py
+++ src/data_formatting.py
@@ -3,10 +3,11 @@ import pandas as pd
 from datetime import datetime
 
 from .db.models.player import Player
+from .db.models.tournament import Tournament
 
 
 def format_player(row: object, fname: str = 'X', lname: str = 'X.1', nationality: str = 'UNK', dob: str = '19000000', hand: str = 'U') -> Player:
-    """Given row from jeff sackmans player csv formats to player objetc
+    """Given row from jeff sackmans player csv formats to player object
 
     Args:
         row (object): DataFrame row 
@@ -24,3 +25,24 @@ def format_player(row: object, fname: str = 'X', lname: str = 'X.1', nationality
                   nationality=row[nationality],
                   dob=datetime.strptime(str(row[dob]), '%Y%m%d'),
                   hand=row[hand])
+
+
+def format_tournament(row: object, name: str = 'tourney_name', surface: str = 'surface', draw_size: str = 'draw_size', level: str = 'tourney_level', start_date: str = 'tourney_date') -> Tournament:
+    """Given row from jeff sackmans matches csv formats to tournament object
+
+    Args:
+        row (object): DataFrame row
+        name (str, optional): column containing tournament name. Defaults to 'tourney_name'.
+        surface (str, optional): column containing surface type. Defaults to 'surface'.
+        draw_size (str, optional): column containing draw size. Defaults to 'draw_size'.
+        level (str, optional): column containing tournament level. Defaults to 'tourney_level'.
+        start_date (str, optional): column containing tournament start date. Defaults to 'tourney_date'.
+
+    Returns:
+        Tournament: tournament instance
+    """
+    return Tournament(name=row[name],
+                      surface=row[surface],
+                      draw_size=row[draw_size],
+                      level=row[level],
+                      start_date=datetime.strptime(str(row[start_date]), '%Y%m%d'))
--- tests/test_data_formatting.py
+++ tests/test_data_formatting.py
@@ -1,8 +1,9 @@
 import pytest
 from datetime import datetime
 
-from src.data_formatting import format_player
+from src.data_formatting import format_player, format_tournament
 from src.db.models.player import Player
+from src.db.models.tournament import Tournament
 
 
 @pytest.mark.slow
@@ -17,3 +18,16 @@ def test_format_player(sample_players):
     assert int(player.dob.strftime('%Y%m%d')) == sample_players.loc[0, '19000000']
     assert isinstance(player.dob, datetime)
     assert player.hand == sample_players.loc[0, 'U']
+
+
+@pytest.mark.slow
+def test_format_tournament(sample_games):
+    tournament = format_tournament(sample_games.iloc[0])
+
+    assert isinstance(tournament, Tournament)
+    assert tournament.name == sample_games.loc[0, 'tourney_name']
+    assert tournament.surface == sample_games.loc[0, 'surface']
+    assert tournament.draw_size == sample_games.loc[0, 'draw_size']
+    assert tournament.level == sample_games.loc[0, 'tourney_level']
+    assert int(tournament.start_date.strftime('%Y%m%d')) == sample_games.loc[0, 'tourney_date']
+    assert isinstance(tournament.start_date, datetime)
