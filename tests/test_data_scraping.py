8b18ee0c974d1da7e3afd808544dc1110dcd8e18
gavin_tynan <gavin.tynan1@gmail.com>
2020-09-03 10:38:22 +0100

    sample data now fixture

--- tests/test_data_scraping.py
+++ tests/test_data_scraping.py
@@ -7,20 +7,17 @@ from src.data_scraping import get_raw_players, get_raw_games
 
 
 @pytest.mark.slow
-def test_get_raw_players():
-    players = get_raw_players(n_players=5)
+def test_get_raw_players(sample_players):
 
-    assert len(players) == 5
-    assert isinstance(players, pd.DataFrame)
+    assert len(sample_players) == 5
+    assert isinstance(sample_players, pd.DataFrame)
 
 
 @pytest.mark.slow
-def test_get_raw_games():
-    year = 2020
-    games = get_raw_games(year, year, 5)
+def test_get_raw_games(sample_games):
 
-    assert len(games) == 5
-    assert isinstance(games, pd.DataFrame)
+    assert len(sample_games) == 5
+    assert isinstance(sample_games, pd.DataFrame)
     # ensure first and last row's date correct year
-    assert datetime.strptime(str(games.loc[0, 'tourney_date']), '%Y%m%d').year == year
-    assert datetime.strptime(str(games.loc[games.index[-1], 'tourney_date']), '%Y%m%d').year == year
+    assert datetime.strptime(str(sample_games.loc[0, 'tourney_date']), '%Y%m%d').year == 2020
+    assert datetime.strptime(str(sample_games.loc[sample_games.index[-1], 'tourney_date']), '%Y%m%d').year == 2020
