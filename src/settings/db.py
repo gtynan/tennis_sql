d362461a84f7c2a66967282f2875beb8724372c5
gavin_tynan <gavin.tynan1@gmail.com>
2020-09-12 13:11:58 +0100

    tidied

--- src/settings/db.py
+++ src/settings/db.py
@@ -5,21 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-DATABASE: str = os.getenv("DATABASE")
-DB_USER: str = os.getenv("DB_USER")
-DB_PASSWORD: str = os.getenv("DB_PASSWORD")
-DB_HOST: str = os.getenv("DB_HOST")
-DB_PORT: str = os.getenv("DB_PORT")
-
-
 DBConfig = TypedDict('DBConfig',
                      {'db_host': str, 'db_port': int, 'db_user': str, 'db_pwd': str, 'db_name': str, })
 
 
 DB_CONFIG: DBConfig = {
-    'db_host': DB_HOST,
-    'db_port': DB_PORT,
-    'db_user': DB_USER,
-    'db_pwd': DB_PASSWORD,
-    'db_name': DATABASE
+    'db_host': os.getenv("DB_HOST"),
+    'db_port': os.getenv("DB_PORT"),
+    'db_user': os.getenv("DB_USER"),
+    'db_pwd': os.getenv("DB_PASSWORD"),
+    'db_name': os.getenv("DATABASE")
 }
