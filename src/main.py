95487cd32494e1246d49496c7698bcd6b56943db
gavin_tynan <gavin.tynan1@gmail.com>
2020-09-16 11:00:05 +0100

    updated to db changes

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ from typing import Generator
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import sessionmaker, Session
 
-from .db.schema.player import PlayerSchema
-from .db.schema.tournament import TournamentSchema
-from .db.schema.game import GameSchema
+from .db.schema.player import PlayerSchema, PlayerTable
+from .db.schema.tournament import TournamentSchema, TournamentTable
+from .db.schema.game import GameSchema, GameTable
 from .db.db import DBClient, QueryDB
 
-DBClient().generate_schema()
+
 app = FastAPI()
+
+DBClient().generate_schema()
 
 
 # dependency
@@ -22,7 +24,7 @@ def get_query_db() -> Generator[QueryDB, None, None]:
 
 @app.get('/player/{player_id}', response_model=PlayerSchema)
 def read_player_by_id(player_id: int, db: QueryDB = Depends(get_query_db)):
-    player = db.get_player_by_id(player_id)
+    player = db.get_object_by_id(player_id, PlayerTable)
     if player is None:
         raise HTTPException(status_code=404, detail='Player not found')
     return player
@@ -30,7 +32,7 @@ def read_player_by_id(player_id: int, db: QueryDB = Depends(get_query_db)):
 
 @app.get('/tournament/{tourney_id}', response_model=TournamentSchema)
 def read_tournament_by_id(tourney_id: str, db: QueryDB = Depends(get_query_db)):
-    tournament = db.get_tournament_by_id(tourney_id)
+    tournament = db.get_object_by_id(tourney_id, TournamentTable)
     if tournament is None:
         raise HTTPException(status_code=404, detail='Tournament not found')
     return tournament
@@ -38,7 +40,7 @@ def read_tournament_by_id(tourney_id: str, db: QueryDB = Depends(get_query_db)):
 
 @app.get('/game/{game_id}', response_model=GameSchema)
 def read_game_by_id(game_id: str, db: QueryDB = Depends(get_query_db)):
-    game = db.get_game_by_id(game_id)
+    game = db.get_object_by_id(game_id, GameTable)
     if game is None:
         raise HTTPException(status_code=404, detail='Game not found')
     return game
