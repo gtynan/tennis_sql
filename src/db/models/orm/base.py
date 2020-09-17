8e20d07b46028424c7f0b2d11ed0e3e1c10a32f1
gavin_tynan <gavin.tynan1@gmail.com>
2020-09-17 15:00:28 +0100

    custom base

--- src/db/models/orm/base.py
+++ src/db/models/orm/base.py
@@ -1,13 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy import Column, Integer
 
-SQLAlchemyBase: DeclarativeMeta = declarative_base()
 
-
-class Base(SQLAlchemyBase):
+class _Base:
     """SQL Alchemy custom base table to ensure all tables have an id column
     """
     id = Column(Integer, primary_key=True, autoincrement=True)
 
 
-Base = declarative_base(cls=Base)
+Base: DeclarativeMeta = declarative_base(cls=_Base)
