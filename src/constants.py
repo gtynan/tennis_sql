4712f1576ee282144523d30015d462744d517c79
gavin_tynan <gavin.tynan1@gmail.com>
2020-09-25 16:29:55 +0100

    commented out ITF

--- src/constants.py
+++ src/constants.py
@@ -3,11 +3,11 @@ from datetime import datetime
 # scraping urls
 PLAYER_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_players.csv"
 WTA_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_{}.csv"
-ITF_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_qual_itf_{}.csv"
+ITF_URL = None  # "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_qual_itf_{}.csv"
 
 SOURCE_COL = 'source'
 
-WTA_IDENTIFIER = 'wta'
+WTA_IDENTIFIER = 'WAT'
 ITF_IDENTIFIER = 'ITF'
 
 SCRAPING_HEADER = {
