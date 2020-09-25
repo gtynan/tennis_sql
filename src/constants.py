d6bf9607123e290abeff9dabf6746a87296f217e
gavin_tynan <gavin.tynan1@gmail.com>
2020-09-25 17:01:39 +0100

    include iTF

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ from datetime import datetime
 # scraping urls
 PLAYER_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_players.csv"
 WTA_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_{}.csv"
-ITF_URL = None  # "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_qual_itf_{}.csv"
+ITF_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_qual_itf_{}.csv"
 
 SOURCE_COL = 'source'
 
@@ -14,5 +14,5 @@ SCRAPING_HEADER = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
 }
 
-INGEST_YEAR_FROM = 1967
+INGEST_YEAR_FROM = 1968
 INGEST_YEAR_TO = datetime.now().year
