8c94f6a9d58844e9fcf15ea2dfd9f6d7b4c7ac27
gavin_tynan <gavin.tynan1@gmail.com>
2020-10-02 13:04:36 +0100

    functions no longer used to speed up ingestion

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 import pytest
+import pandas as pd
 
 from src.db.db import DBClient
 from src.db.models.orm.base import Base
-from src.data.data_scraping import get_raw_players, get_raw_games
+from src.constants import PLAYER_URL, GAME_SOURCES, WTA_IDENTIFIER
 
 
 @pytest.fixture(scope='session')
@@ -21,9 +22,11 @@ def db_client():
 @pytest.fixture(scope='session')
 def sample_players():
     """Gets 5 sample WTA players"""
-    return get_raw_players().iloc[:5]
+    return pd.read_csv(PLAYER_URL, nrows=5)
 
 
 @pytest.fixture(scope='session')
 def sample_games():
-    return get_raw_games(2020, 2020).head()
+    """Gets 5 sample WTA players"""
+    games_2020_url = GAME_SOURCES[WTA_IDENTIFIER].format(2020)
+    return pd.read_csv(games_2020_url, nrows=5)
