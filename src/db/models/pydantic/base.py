b1ab4e1f2d602fd030ded4b2f8cc0e5e8cd50abd
gavin_tynan <gavin.tynan1@gmail.com>
2020-10-05 16:59:38 +0100

    moved comparison to base

--- src/db/models/pydantic/base.py
+++ src/db/models/pydantic/base.py
@@ -6,6 +6,13 @@ import numpy as np
 class BaseModel(Base):
     id: Optional[Union[str, int]]
 
+    # allows comparisons via np.unique()
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __lt__(self, other):
+        return self.id < other.id
+
     class Config:
         orm_mode = True
 
@@ -20,10 +27,3 @@ class CreateModel(Base):
         if (field.outer_type_ is str and v == 'nan') | (field.outer_type_ is float and np.isnan(v)):
             return None
         return v
-
-    # allows comparisons via np.unique()
-    def __eq__(self, other):
-        return self.id == other.id
-
-    def __lt__(self, other):
-        return self.id < other.id
