65ebdb004b29a91a58fbb6dffa2883d72e43c639
gavin_tynan <gavin.tynan1@gmail.com>
2020-10-05 16:59:01 +0100

    added match_num

--- src/db/models/orm/game.py
+++ src/db/models/orm/game.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, ForeignKey, String, Integer
 from sqlalchemy.orm import relationship
 
 from .base import Base
@@ -12,11 +12,12 @@ class Game(Base):
 
     id = Column(String(50), primary_key=True)
 
+    round = Column(String(50))
+    score = Column(String(50))
+    match_num = Column(Integer)
+
     tournament_id = Column(String(50), ForeignKey('tournament.id'))
     tournament = relationship("Tournament", uselist=False, foreign_keys=[tournament_id], back_populates="games")
 
     w_performance = relationship("WPerformance", uselist=False)
     l_performance = relationship("LPerformance", uselist=False)
-
-    round = Column(String(50))
-    score = Column(String(50))
